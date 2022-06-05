9964a874cd45d35b88b01c85169ffb5802d44c07
Guilherme Lavezzo <guilherm909088@gmail.com>
2022-06-04 22:53:55 -0300

    Upando primeiro componente

---
+++ angular-part1/src/app/nova-tranferencia/nova-tranferencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-nova-tranferencia',
+    templateUrl:'./nova-tranferencia.component.html',
+    styleUrls:['./nova-tranferencia.component.scss']
+
+})
+
+export class novaTranferenciaComponent{
+
+}
